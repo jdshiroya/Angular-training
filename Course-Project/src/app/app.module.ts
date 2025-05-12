7a5e121b64ab34d06a69acd7d785bbc5e16772ce
JayminShiroya07 <jaymin.shiroya@seaflux.tech>
2025-05-12 11:14:11 +0530

    feat : complete directives topic

--- Course-Project/src/app/app.module.ts
+++ Course-Project/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeListComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],

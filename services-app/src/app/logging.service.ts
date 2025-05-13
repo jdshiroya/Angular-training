e0727099076bbed5b2d072c12df6bb4cfd48569e
JayminShiroya07 <jaymin.shiroya@seaflux.tech>
2025-05-13 14:33:39 +0530

    feat : service section completed

---
+++ services-app/src/app/logging.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'platform'
+})
+export class LoggingService {
+
+  logStatusChange(status:string){
+    console.log('A server status changed, new status: ' + status);
+  }
+  constructor() { }
+}

375fb44de5dc0a4ecb41c75aa65761003443d0bf
JayminShiroya07 <jaymin.shiroya@seaflux.tech>
2025-05-16 17:48:54 +0530

    feat : observables section completed

--- Observables/src/app/home/home.component.ts
+++ Observables/src/app/home/home.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count > 3){
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      filter((data:number) => data%2 !== 0 ? true : false),
+      map((data: number) => {
+        return 'Round: ' + (data + 1);
+      })
+    ).subscribe(data => {
+      console.log(data);
+    },error => {
+      console.log(error);
+      alert(error.message);
+    }
+  );
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+}
+function handler(args_0: any, args_1: number): void {
+  throw new Error('Function not implemented.');
 }
--- Observables/src/app/user/user.component.ts
+++ Observables/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +9,29 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   id: number;
+  activeStatus : boolean;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private userService : UserService
+  ) {
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+    this.userService.activatedEmitter.next(false);
+  }
+
+  onActivate(){
+    this.userService.activatedEmitter.next(true);
+  }
+
+  toggleActiveStatus(){
+    this.userService.activatedEmitter.subscribe((status)=>{
+      this.activeStatus = status;
+    });
+    this.userService.activatedEmitter.next(!this.activeStatus);  
   }
 }

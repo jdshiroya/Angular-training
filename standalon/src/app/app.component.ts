1c23da848a67487cf9db05a6933d39f9db3ec112
JayminShiroya07 <jaymin.shiroya@seaflux.tech>
2025-06-02 17:52:50 +0530

    feat : update course project

--- standalon/src/app/app.component.ts
+++ standalon/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
+
 @Component({
+  standalone: true,
+  imports: [RouterModule, WelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
